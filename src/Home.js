1b2aa9f6689615d01522a546a6de796274d643e7
ZeroDawn0D <umang26112003@gmail.com>
2020-09-19 23:34:27 +0530

    a

--- src/Home.js
+++ src/Home.js
@@ -34,8 +34,8 @@ class UserAuth extends React.Component
 		return(
 			<div class = "user-auth">
 				<input
-				placeholder = "Email"
-				type = "email"
+				placeholder = "Username"
+				type = "text"
 				value = {this.state.email}
 				onChange = {this.handleEmail}/>
 				
@@ -92,7 +92,9 @@ class Body extends React.Component{
 					<img class="poll-img" src="/polls.png"></img>
 				</div>
 
-				<Bar />
+			<Poll />
+
+
 			</div>
 		);
 	}
@@ -102,13 +104,144 @@ class Bar extends React.Component{
 	constructor(props)
 	{
 		super(props);
+		console.log(this.props);
 	}
 	render()
 	{
+		let p = this.props.percent;
+		let message = this.props.percent;
+		if(p === " ")
+		{
+			p = "0%";
+			message = " "
+		}
 		return(
 			<div class = "container">
+				<div style = {{
+					textAlign:"left",
+					paddingLeft: "3px",
+					minHeight:"100%",
+					borderRadius: "10px",
+					borderBottomRightRadius:"10px",
+					width:p,
+					backgroundColor:'#ef514d'}}>
+						{message}
+				</div>
 			</div>
 		);
+	}
+
+}
+
+
+class Poll extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state ={
+			question: "Cats or Dogs?",
+			desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+			id: "ID",
+			total: 5,
+			votes: [2,3],
+			names: ["Woof!", "Nya?"],
+			percent: [],
+			hasVoted:false
+		}
+		let newPercent = new Array(this.state.votes.length);
+		let i = 0;
+		for(;i<this.state.votes.length;i++)
+		{
+			newPercent[i] = this.getPercent(this.state.votes[i]);
+		}
+		console.log(newPercent);
+		this.setState({
+			percent: newPercent
+		});
+
+		this.handleVote = this.handleVote.bind(this);
+	}
+
+	printVoteList()
+	{
+		const pList = this.state.names.map((n,index) =>
+			<div className = "option" key = {index} onClick={(e)=>this.handleVote(index,e)} >
+				{n}
+				<Bar percent = " " />
+
+			</div>
+		);
+
+		return(pList);
+	}
+	async handleVote(index,e)
+	{
+		let newVotes = this.state.votes;
+		newVotes[index]++;
+		await this.setState({
+			total: this.state.total+1
+		})
+		let newPercent = new Array(this.state.votes.length);
+		let i = 0;
+		for(;i<this.state.votes.length;i++)
+		{
+			newPercent[i] = this.getPercent(this.state.votes[i]);
+		}
+		this.setState({
+			hasVoted:true,
+			percent: newPercent,
+			votes:newVotes
+		})
+	}
+	printVotedList()
+	{
+		const pList = this.state.names.map((n,index) =>
+			<div className = "option" key = {index}>
+				<div>
+					<span>{n}</span>
+					<Bar percent = {this.state.percent[index] + "%"} />
+				</div>
+				
+			</div>
+		);
+
+		return(pList);
+	}
+	getPercent(n)
+	{
+		return (100*n/this.state.total).toFixed(2);
+	}
+	render()
+	{
+			if(this.state.hasVoted)
+				return(
+					<div className = "poll">
+						<div class = "question">
+							{this.state.question}
+						</div>
+						
+						{this.printVotedList()}
+
+						<div class = "poll-desc">
+							{this.state.desc}
+						</div>
+					</div>
+				);
+			else
+				return(
+					<div className = "poll">
+						<div class = "question">
+							{this.state.question}
+						</div>
+						
+						{this.printVoteList()}
+
+						<div class = "poll-desc">
+							{this.state.desc}
+						</div>
+					</div>
+				);
 	}
 
 }
