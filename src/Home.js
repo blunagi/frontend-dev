877d72f38101efae1d1e72999aad30ea2725c008
ZeroDawn0D <umang26112003@gmail.com>
2020-09-19 17:47:24 +0530

    a

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 
-class Poll extends React.Component
+class PollBox extends React.Component
 {
 	//props: poll_name, poll_desc
 	constructor(props)
@@ -11,14 +11,14 @@ class Poll extends React.Component
 	render()
 	{
 		return(
-			<div className = "poll">
-				<div className = "poll-head">
+			<div className = "pollbox">
+				<div className = "pollbox-head">
 					{this.props.poll_name}
 				</div>
 				
 				<hr />
 
-				<div className = "poll-desc">
+				<div className = "pollbox-desc">
 					{this.props.poll_desc}
 				</div>
 			</div>
@@ -26,7 +26,7 @@ class Poll extends React.Component
 	}
 }
 
-class Polls extends React.Component
+class PollBoxes extends React.Component
 {
 	constructor(props)
 	{
@@ -35,15 +35,83 @@ class Polls extends React.Component
 	render()
 	{
 		return(
-			<div id = "polls">
-				<Poll poll_name = "Poll 1" poll_desc = "DESC1" />
-				<Poll poll_name = "Poll 2" poll_desc = "DESC2" />
-				<Poll poll_name = "Poll 3" poll_desc = "DESC3" />
+			<div id = "pollboxes">
+				<PollBox poll_name = "Poll 1" poll_desc = "DESC1" />
+				<PollBox poll_name = "Poll 2" poll_desc = "DESC2" />
+				<PollBox poll_name = "Poll 3" poll_desc = "DESC3" />
 			</div>
 			);
 	}
 }
 
+class UserAuth extends React.Component
+{
+	constructor(props)
+	{
+		super(props);
+		this.state = {
+			email: '',
+			pw: ''
+		};
+
+		this.handleEmail = this.handleEmail.bind(this);
+		this.handlePw = this.handlePw.bind(this);
+	}
+
+	async handleEmail(event)
+	{
+		await this.setState({
+			email: event.target.value
+		})
+	}
+	async handlePw(event)
+	{
+		await this.setState({
+			pw: event.target.value
+		})
+	}
+	render()
+	{
+		return(
+			<div class = "user-auth">
+				<input
+				placeholder = "Email"
+				type = "email"
+				value = {this.state.email}
+				onChange = {this.handleEmail}/>
+				
+				<input
+				placeholder = "Password"
+				type = "password"
+				value = {this.state.pw}
+				onChange = {this.handlePw}/>
+				
+				<span class = "login">
+				Login
+				</span>
+
+				<span>
+				Sign-up
+				</span>
+			</div>
+			);
+	}
+}
+
+class Top extends React.Component
+{
+
+	render()
+	{
+		return(
+		<div id = "top">
+			<div id = "header">
+				<img src="/logo.png"></img>
+			</div>
+			<UserAuth />
+		</div>);
+	}
+}
 class Home extends React.Component
 {
 	constructor(props)
@@ -54,11 +122,9 @@ class Home extends React.Component
 	{
 		return(
 			<div>
-				<div id = "header">
-					<img src="/logo.png"></img>
-				</div>
+				<Top />
 
-				<Polls />
+				<PollBoxes />
 			</div>);
 	}
 }
